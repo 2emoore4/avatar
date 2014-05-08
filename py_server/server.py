68250a5d992d8c86af9e9282cc2b86e97e2ba03b
Miles Steele <miles@milessteele.com>
2014-05-08 15:03:02 -0400

    add locking between threads (break everything)

--- py_server/server.py
+++ py_server/server.py
@@ -24,7 +24,7 @@ data_queue = Queue.Queue(maxsize=0) # maxsize=0 means unlimited capacity
 # these are read and written to the arduino as they arrive on the queue
 # format: (pump_power,)
 arduino_state = Queue.Queue(maxsize=0)
-arduino_state.put(('a',))
+arduino_state.put((0,))
 
 # a list of known substrings of serial addresses to try to connect to.
 # if None, then skip connecting
@@ -35,8 +35,13 @@ arduino_serial = None
 # response handler for web sockets
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
-        print("received a message: %s" % (message))
+        print("received a message: {}".format(message))
         obj = json.loads(message)
+
+        # wait until all data has been read
+        while not data_queue.empty():
+            pass
+
         data_queue.put(obj)
 
 app = tornado.web.Application([
@@ -47,10 +52,24 @@ app = tornado.web.Application([
 def process_data():
     print "process_data started"
     while True:
+        # wait until all states have been written
+        if not arduino_state.empty():
+            continue
+
         # block on new data
         newdata = data_queue.get()
-        print("new data processed!", newdata)
-        state = ('m',)
+        print("processing new data: {}".format(newdata))
+        dtype = newdata['type']
+        dval = newdata['value']
+
+        pump_power = 0
+
+        if newdata['type'] == 'audio-volume':
+            baseline = -160
+            max_delta = 30
+            pump_power = (dval - baseline) / max_delta
+
+        state = (pump_power,)
         arduino_state.put(state)
 
 def write_to_arduino():
@@ -59,8 +78,18 @@ def write_to_arduino():
         # block on new data
         state = arduino_state.get()
         print("writing to arduino", state)
-        for val in state:
-            arduino_serial.write(val)
+        if arduino_serial != None:
+            for val in state:
+                arduino_serial.write(convert_float_letter(val))
+
+
+# convert a float to a letter in [a, z]
+# ['a', 'z'] maps to [0, 1]
+# anything floats out of bounds will be clamped. So 1.7 is 'z'
+def convert_float_letter(num):
+    num = max(0, min(float(num), 1))
+    range_width = ord('z') - ord('a')
+    return chr(ord('a') + int(num * range_width))
 
 
 # Provides all serial port names as strings
@@ -93,16 +122,6 @@ def connect_to_serial(serial_addr_subs):
                 #     continue
     return None # if no matches
 
-# Initializing arduino connection
-if serial_addr_subs != None:
-    print("Initializing arduino connection...")
-    arduino_serial = connect_to_serial(serial_addr_subs)
-    if arduino_serial == None:
-        print("COULD NOT FIND ANY MATCHING SERIAL PORTS")
-else:
-    print("Skipping arduino connection")
-    arduino_serial = None
-
 
 def start_daemon_thread(fn):
     thread = threading.Thread(target=fn)
@@ -112,10 +131,24 @@ def start_daemon_thread(fn):
 
 if __name__ == "__main__":
     try:
+        # Initializing arduino connection
+        if serial_addr_subs != None:
+            print("Initializing arduino connection...")
+            arduino_serial = connect_to_serial(serial_addr_subs)
+            if arduino_serial == None:
+                print("\n\n")
+                print("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ")
+                print("COULD NOT FIND ANY MATCHING SERIAL PORTS")
+                print("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ")
+                print("\n\n")
+        else:
+            print("Skipping arduino connection")
+            arduino_serial = None
+
         parse_command_line()
         app.listen(options.port)
-        start_daemon_thread(process_data)
         start_daemon_thread(write_to_arduino)
+        start_daemon_thread(process_data)
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
         print "GOODBYE"
