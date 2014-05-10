290f56829b48c9abe2ec663ad8cf8104a216b7c1
Evan Moore <2emoore4@gmail.com>
2014-05-09 22:35:10 -0400

    python sensor client reads 'packets' sent from arduino

--- arduino/input_sensors/src/sketch.ino
+++ arduino/input_sensors/src/sketch.ino
@@ -1,7 +1,32 @@
+#define LED_N 2
+#define LED_P 3
+
 void setup() {
     Serial.begin(9600);
-    Serial.println("Hello.");
 }
 
 void loop() {
+    //Serial.println(read_light());
+    Serial.print("a");
+    Serial.print("b");
+    Serial.print("c");
+}
+
+int read_light() {
+    unsigned int j;
+
+    pinMode(LED_N, OUTPUT);
+    pinMode(LED_P, OUTPUT);
+    digitalWrite(LED_N, HIGH);
+    digitalWrite(LED_P, LOW);
+
+    pinMode(LED_N, INPUT);
+    digitalWrite(LED_N, LOW);
+
+    for (j = 0; j < 3000; j++) {
+        if (digitalRead(LED_N) == 0) break;
+        delay(10);
+    }
+
+    return j;
 }
--- py_client/sensors.py
+++ py_client/sensors.py
@@ -34,23 +34,43 @@ def connect_to_serial(serial_addr_subs):
                 ser = serial.Serial(port)
                 print("Conneced to serial port: " + ser.port)
                 return ser
-                # ser.timeout = 0.5
-                # ser.write('c')
-                # msg = ser.read()
-                # if msg == 'y':
-                #     continue
     return None # if no matches
 
-# Initializing arduino connection
-if serial_addr_subs != None:
-    print("Initializing arduino connection...")
-    arduino_serial = connect_to_serial(serial_addr_subs)
-    if arduino_serial == None:
-        print("COULD NOT FIND ANY MATCHING SERIAL PORTS")
-else:
-    print("Skipping arduino connection")
-    arduino_serial = None
-
 # connect to websocket
-websocket = create_connection("ws://18.238.6.29:8080")
-websocket.send(json.dumps({'type': 'light', 'value': 0.5}))
+#websocket = create_connection("ws://18.238.6.29:8080")
+#websocket.send(json.dumps({'type': 'light', 'value': 0.5}))
+
+def next_packet():
+    while True:
+        msg = arduino_serial.read()
+        if msg == 'a':
+            return (arduino_serial.read(), arduino_serial.read())
+
+def start_daemon_thread(fn):
+    thread = threading.Thread(target=fn)
+    thread.daemon = True
+    thread.start()
+
+if __name__ == "__main__":
+    try:
+        # Initializing arduino connection
+        if serial_addr_subs != None:
+            print("Initializing arduino connection...")
+            arduino_serial = connect_to_serial(serial_addr_subs)
+            if arduino_serial == None:
+                print("\n\n")
+                print("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ")
+                print("COULD NOT FIND ANY MATCHING SERIAL PORTS")
+                print("! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ")
+                print("\n\n")
+                raw_input("Press <enter> if this is ok.\n")
+        else:
+            print("Skipping arduino connection.")
+            arduino_serial = None
+
+        while True:
+            print next_packet()
+
+    except KeyboardInterrupt:
+        print "GOODBYE"
+        sys.exit()
